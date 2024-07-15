0ff51363666a903e58309ea0b4c351b36b844658
Rebeca Prado <rebeca.prado.dev@gmail.com>
2024-07-15 16:22:16 -0300

    test: add unit test initialBinding for Section Data

--- test/layers/presentation/bindings/bindings_test.dart
+++ test/layers/presentation/bindings/bindings_test.dart
@@ -1,6 +1,9 @@
 import 'package:elo7_app/layers/data/datasources/jobs_datasource.dart';
+import 'package:elo7_app/layers/data/datasources/section_data_datasource.dart';
 import 'package:elo7_app/layers/domain/repositories/jobs_repository.dart';
+import 'package:elo7_app/layers/domain/repositories/section_data_repositoy.dart';
 import 'package:elo7_app/layers/domain/usecases/jobs/jobs_usecase.dart';
+import 'package:elo7_app/layers/domain/usecases/section_data/section_data_usecase.dart';
 import 'package:elo7_app/layers/presentation/bindings/initial_binding.dart';
 import 'package:elo7_app/layers/presentation/controller/home_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -12,6 +15,10 @@ void main() {
       final initialBinding = InitialBinding();
 
       initialBinding.dependencies();
+
+      expect(Get.isRegistered<SectionDataDatasource>(), true);
+      expect(Get.isRegistered<SectionDataRepositoy>(), true);
+      expect(Get.isRegistered<SectionDataUsecase>(), true);
 
       expect(Get.isRegistered<JobsDatasource>(), true);
       expect(Get.isRegistered<JobsRepository>(), true);
